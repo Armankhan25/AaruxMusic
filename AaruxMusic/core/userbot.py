7d87f7fb9680df75e338c1b1779b7dff74979e2b
Armankhan25 <sonicxMusic25@gmail.com>
2026-08-30 18:55:36 +0530

    Update userbot.py

--- AaruxMusic/core/userbot.py
+++ AaruxMusic/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="DevSpAss1",
+            name="AaruxMusic1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="DevSpAss2",
+            name="AaruxMusic2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="DevSpAss3",
+            name="AaruxMusic3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="DevSpAss4",
+            name="AaruxMusic4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="DevSpAss5",
+            name="AaruxMusic5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
