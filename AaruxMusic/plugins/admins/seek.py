bfb1a1629053d6592194595a124c9150d438c9e1
Armankhan25 <sonicxMusic25@gmail.com>
2026-08-30 19:34:54 +0530

    Replace DevSp with Aaru in seek.py

--- AaruxMusic/plugins/admins/seek.py
+++ AaruxMusic/plugins/admins/seek.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AaruxMusic import YouTube, app
-from AaruxMusic.core.call import DevSp
+from AaruxMusic.core.call import Aaru
 from AaruxMusic.misc import db
 from AaruxMusic.utils import AdminRightsCheck, seconds_to_min
 from AaruxMusic.utils.inline import close_markup
@@ -56,7 +56,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
     try:
-        await DevSp.seek_stream(
+        await Aaru.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
