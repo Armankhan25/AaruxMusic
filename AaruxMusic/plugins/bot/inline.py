8a0eaee313d122b66fe8c8858ac23cf077a88ecd
Armankhan25 <sonicxMusic25@gmail.com>
2026-08-30 19:37:08 +0530

    Update inline.py

--- AaruxMusic/plugins/bot/inline.py
+++ AaruxMusic/plugins/bot/inline.py
@@ -36,7 +36,7 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʏᴏᴜᴛᴜʙᴇ 🎄",
+                            text="ʏᴏᴜᴛᴜʙᴇ 🌷",
                             url=link,
                         )
                     ],
