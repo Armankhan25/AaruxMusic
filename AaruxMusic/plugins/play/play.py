b4a0ba66e782ea0da0cdf530882374a2baa316d9
Armankhan25 <sonicxMusic25@gmail.com>
2026-08-31 10:11:13 +0530

    Update play.py

--- AaruxMusic/plugins/play/play.py
+++ AaruxMusic/plugins/play/play.py
@@ -510,7 +510,7 @@ async def anonymous_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("DevSpPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("AaruPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
